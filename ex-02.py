410c43fe48a440197aa6f357665415e1f472d0f7
Mister2Tone <pisit_tansri@hotmail.com>
2017-09-07 02:23:15 +0700

    add excercies 2

---
+++ ex-02.py
@@ -0,0 +1,37 @@
+import os
+
+file = open('./student.txt')
+studentDict = {}
+
+for line in file:
+	splitLine = line.split('=')
+	studentDict[splitLine[0]] = splitLine[1].strip('\n')
+
+file.close()
+
+studentId = list(studentDict.keys())
+studentGroup = list(studentDict.values())
+
+for i in range(len(studentId)):
+	dirName = studentId[i]
+	group = studentGroup[i]
+
+	rootDir = os.getcwd()
+
+	if os.path.isdir(dirName) :
+		os.chdir(dirName)
+	else :
+		os.mkdir(dirName)
+		os.chdir(dirName)
+
+	checkDir = ("{0}\{1}".format(rootDir,dirName))
+
+	if(os.getcwd() == checkDir):
+		file = open("work.conf", "w")
+		file.write("{0}={1}".format(dirName,group))
+		file.close()
+		os.chdir("..")
+	else:
+		print("wrong, please check your path directory")
+
+
