c725d3eba4f212dff7e3be052c4944953e905291
Mister2Tone <pisit_tansri@hotmail.com>
2017-09-06 21:40:20 +0700

    add exercies 1

---
+++ ex-01.py
@@ -0,0 +1,27 @@
+import operator
+
+file = open('./employee.txt')
+employeeDict = {}
+
+for line in file:
+	splitLine = line.split(':')
+	employeeDict[splitLine[0]] = int(splitLine[1].strip('\n'))
+
+file.close()
+
+salarySorted = sorted(employeeDict.items(), key=operator.itemgetter(1), reverse=True )
+
+print(employeeDict)
+print("----------------")
+print("Sum of salary: ",sum(employeeDict.values() ))
+print("Salary by Ascending:")
+employeeSortedDict = dict(salarySorted)
+employeeName = list(employeeSortedDict.keys())
+employeeSalary = list(employeeSortedDict.values())
+
+for i in range(len(employeeName)):
+	# print("{1} : {2}".format(employeeName[i],employeeSalary[i]))
+	print(employeeName[i],":",employeeSalary[i])
+
+
+
